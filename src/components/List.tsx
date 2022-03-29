e43404dd054073e69b2b7a0ebb1e9343514f43eb
Mette Hansen <mettehansen@grundfos.com>
2022-03-29 15:40:52 +0200

    refactor list component

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,18 +1,25 @@
-import React from 'react';
+import React from "react";
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
+import { Task } from "../types";
+import TaskCard from "./TaskCard";
 
-interface ListProps{
-  children: JSX.Element | JSX.Element[];
+interface ListProps {
+  list: Task[];
+  listType: "BACKLOG" | "DOING" | "DONE";
   className?: string;
 }
 //get status and filter based on that
-  const List = ({children, className}:ListProps)=>{
-    return(
-      <div className={className}>
-        {children}
-      </div>
-    )
-  }
+const List = ({ list, className, listType }: ListProps) => {
+  return (
+    <div className={className}>
+      {list
+        .filter((item) => item.status === listType)
+        .map((task) => (
+          <TaskCard key={task.id}>{task.name}</TaskCard>
+        ))}
+    </div>
+  );
+};
 
-  export default List;
+export default List;
